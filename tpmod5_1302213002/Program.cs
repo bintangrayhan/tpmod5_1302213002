be49cef5e997d0f02e301251e0a3ea5a837e40e0
bintangrayhan <127047367+bintangrayhan@users.noreply.github.com>
2023-03-19 20:54:22 +0700

    Add project files.

---
+++ tpmod5_1302213002/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<T>(T inputuser)
+        {
+            Console.WriteLine("Hallo user " + inputuser);
+        }
+    }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("bintang rayhan wijaya");
+        DataGeneric<string> nim = new DataGeneric<string>("1302213002");
+        nim.PrintData();
+    }
+}
